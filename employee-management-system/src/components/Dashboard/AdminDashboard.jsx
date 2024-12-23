b24c2b9e6165f3fd84b75181f99592d2f6595d6d
Arslan-Ahmad-12 <arslaan.ahmad2002@gmail.com>
2024-12-23 19:23:21 +0500

    Fourth commint adding components tasklist

--- employee-management-system/src/components/Dashboard/AdminDashboard.jsx
+++ employee-management-system/src/components/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Header from '../others/Header'
 
 const AdminDashboard = () => {
   return (
-    <div>
-      
+    <div className='h-screen w-full p-10'>
+      <Header/>
+      <div>
+        <form>
+          <h3>Task Title</h3>
+          <input type="text" placeholder='Make a UI design'/>
+          <h3>Description</h3>
+          <textarea cols="30" rows="10"></textarea>
+          <h3>Date</h3>
+          <input type="" name="" id="" />
+        </form>
+      </div>
     </div>
   )
 }
--- employee-management-system/src/components/TaskList/TaskList.jsx
+++ employee-management-system/src/components/TaskList/TaskList.jsx
@@ -17,6 +17,48 @@ const TaskList = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ad repellat placeat omnis quod voluptate.
          </p>
         </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl mx-2'>
+         <div className='flex justify-between items-center'>
+         <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>
+                High
+            </h3>
+            <h4 className='text-sm'>
+                23 Dec 2024
+            </h4>
+         </div>
+         <h2 className='mt-5 text-2xl font-semibold'>Make a youtube vidio</h2>
+         <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ad repellat placeat omnis quod voluptate.
+         </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl mx-2'>
+         <div className='flex justify-between items-center'>
+         <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>
+                High
+            </h3>
+            <h4 className='text-sm'>
+                23 Dec 2024
+            </h4>
+         </div>
+         <h2 className='mt-5 text-2xl font-semibold'>Make a youtube vidio</h2>
+         <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ad repellat placeat omnis quod voluptate.
+         </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl mx-2'>
+         <div className='flex justify-between items-center'>
+         <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>
+                High
+            </h3>
+            <h4 className='text-sm'>
+                23 Dec 2024
+            </h4>
+         </div>
+         <h2 className='mt-5 text-2xl font-semibold'>Make a youtube vidio</h2>
+         <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ad repellat placeat omnis quod voluptate.
+         </p>
+        </div>
     </div>
   )
 }
