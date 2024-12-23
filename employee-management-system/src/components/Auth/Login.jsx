7bdb137e683639d86752a8cb1839276057690017
Arslan-Ahmad-12 <arslaan.ahmad2002@gmail.com>
2024-12-23 16:46:16 +0500

    Third commit

---
+++ employee-management-system/src/components/Auth/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    
+     const [email,setEmail]=useState('');
+     const [password,setPassword]=useState('');
+
+    const submitHandler = (e) => {
+       e.preventDefault()
+    }
+
+    return (
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <div className='border-2 border-emerald-600' >
+                <form onSubmit={(e) => {
+                   submitHandler(e)
+                 }}
+                className='flex flex-col items-center justify-center p-20'>
+                    <input type="email"
+                        required
+                        placeholder='enter your email'
+                        className='border-2 border-emerald-600 rounded-full py-4 px-5 text-xl text-white outline-none bg-transparent mt-3'
+                        value={email}
+                        onChange={(e)=>{
+                            setEmail(e.target.value);
+                        }}
+
+                    />
+                    <input type="password"
+                        placeholder='enter password'
+                        className='border-2 border-emerald-600 rounded-full py-4 px-5 text-xl text-white outline-none bg-transparent mt-3'
+                        value={password}
+                        onChange={(e)=>{
+                            setPassword(e.target.value);
+                        }}
+                    />
+                    <button
+                        className='mt-5 border-2 border-none bg-emerald-600 rounded-full py-4 px-5 text-xl text-white outline-none bg-transparent '
+                    >Log in</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
