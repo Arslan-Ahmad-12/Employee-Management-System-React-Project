3c2ae76a42de7dbdc4e700ec9fbbd273fe49fa1b
Arslan-Ahmad-12 <arslaan.ahmad2002@gmail.com>
2024-12-27 16:50:25 +0500

    Admin panel

--- employee-management-system/src/components/Dashboard/AdminDashboard.jsx
+++ employee-management-system/src/components/Dashboard/AdminDashboard.jsx
@@ -4,15 +4,33 @@ import Header from '../others/Header'
 const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-10'>
-      <Header/>
+      <Header />
       <div>
-        <form>
-          <h3>Task Title</h3>
-          <input type="text" placeholder='Make a UI design'/>
-          <h3>Description</h3>
-          <textarea cols="30" rows="10"></textarea>
-          <h3>Date</h3>
-          <input type="" name="" id="" />
+        <form className='flex items-start justify-between w-full bg-red-200'>
+          <div className='w-1/2 '>
+            <div>
+              <h3>Task Title</h3>
+              <input type="text" placeholder='Make a UI design' />
+            </div>
+            <div>
+              <h3>Date</h3>
+              <input type="date" name="" id="" />
+            </div>
+            <div>
+              <h3>Asign to </h3>
+              <input type="text" placeholder='employee name' />
+            </div>
+            <div>
+              <h3>Category </h3>
+              <input type="text" placeholder='design, dev , etc' />
+            </div>
+          </div>
+          <div>
+            <h3>Description</h3>
+            <textarea cols="30" rows="10"></textarea>
+          </div>
+
+          <button>Create Taske</button>
         </form>
       </div>
     </div>
